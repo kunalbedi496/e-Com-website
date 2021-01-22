06248e3767bbf36fca9531d1861223290cd3a5ff
kunalbedi496 <kunalbedi496@gmail.com>
2021-01-22 19:26:21 +0530

    changes done

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -1,6 +1,6 @@
 export const API =
   process.env.NODE_ENV === "production"
-    ? "https://backeend.herokuapp.com/api"
+    ? "https://e-comstore.herokuapp.com/api"
     : process.env.REACT_APP_BACKEND;
 
 // export const API = process.env.REACT_APP_BACKEND;
